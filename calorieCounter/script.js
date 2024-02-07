83f0e49169c15660192620a4126ec9455a5abeed
Alish <46455895+Alish8950@users.noreply.github.com>
2024-02-07 19:55:53 +0530

    Update script.js

--- calorieCounter/script.js
+++ calorieCounter/script.js
@@ -18,5 +18,9 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+  `;
 }
